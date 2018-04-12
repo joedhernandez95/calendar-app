de46a42428eefd944f94d7ac83bb9adac3b21c4b
Jose Hernandez <jose.hernandez@locstatt.net>
2018-04-12 00:41:56 -0600

    starting with logic

--- js/index.js
+++ js/index.js
@@ -1,4 +1,26 @@
 $(document).ready(function() {
 
-	
+	$('.submit').click(function(){
+		var date = $("#start_date").val();
+    	var number_of_days = $("#number_of_days").val();
+    	var country_code = $("#country").val();
+
+
+    	dateArray = date.split('/');
+
+    	month = parseInt(dateArray[0]);
+    	day = parseInt(dateArray[1]);
+    	year = parseInt(dateArray[2]);
+
+    	//Check if leap year
+    	if (((year % 4 === 0) && (year % 100 != 0)) || (year % 400 === 0)){
+    		console.log(true)
+    	}else{
+    		console.log(false)
+    	}
+    	
+    	console.log(month);
+    	console.log(day);
+    	console.log(year);
+	});
 });
